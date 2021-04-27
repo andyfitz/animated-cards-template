cd42796067f645d2428524d03bafbab2d193a0b3
Andy Fitzsimon <andyfitz@gmail.com>
2021-04-27 23:47:03 +1000

    eg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 import './App.css';
-import Shapes from './components/shapes/shapes'
-import Logos from './components/logos/logos'
-import {ReactComponent as MakeLogo} from './logo.svg'
-import {ReactComponent as ReactLogo} from './react-logo.svg'
+ 
+function checknext() {
+  if (document.querySelector(".last:checked")) {
+  } else {
 
 function App({ headline, showLogos, backgroundImage }) {
   return (
@@ -12,25 +12,53 @@ function App({ headline, showLogos, backgroundImage }) {
         <Logos logoOne={<MakeLogo />} logoTwo={<ReactLogo />} />
       )}
 
-      <h1>{headline}</h1>
 
-      <div className="shape-container">
-        <Shapes />
-      </div>
+function App({ post, messageOne, messageTwo, messageThree, messageFour, messageFive, messageSix, messageSeven, messageEight,}) {
+  return (
+<div className="App" >
+<input class="n1 nav first" name="nav" type="radio" checked />
+<input class="n2 nav" name="nav" type="radio"  />
+<input class="n3 nav" name="nav" type="radio"  />
+<input class="n4 nav" name="nav" type="radio"  />
+<input class="n5 nav" name="nav" type="radio"  />
+<input class="n6 nav" name="nav" type="radio"  />
+<input class="n7 nav" name="nav" type="radio"  />
+<input class="n8 nav last" name="nav" type="radio"  />
+
+<section class="s1">{messageOne}</section>
+<section class="s2">{messageTwo}</section>
+<section class="s3">{messageThree}</section>
+<section class="s4">{messageFour}</section>
+<section class="s5">{messageFive}</section>
+<section class="s6">{messageSix}</section>
+<section class="s7">{messageSeven}</section>
+<section class="s8">{messageEight}</section>
     </div>
   );
 }
 
 App.propTypes = {
-  headline: PropTypes.string,
-  showLogos: PropTypes.string,
-  backgroundImage: PropTypes.string,
+  post: PropTypes.string,
+  messageOne: PropTypes.string,
+  messageTwo: PropTypes.string,
+  messageThree: PropTypes.string,
+  messageFour: PropTypes.string,
+  messageFive: PropTypes.string,
+  messageSix: PropTypes.string,
+  messageSeven: PropTypes.string,
+  messageEight: PropTypes.string
 }
 
 App.defaultProps = {
-  headline: 'Hello World',
-  showLogos: 'true',
-  backgroundImage: '',
+  post: '',
+  messageOne: 'your welcome message',
+  messageTwo: 'an introduction',
+  messageThree: 'some more detail',
+  messageFour: 'a nice anecdote',
+  messageFive: 'personal message',
+  messageSix: 'from personal other',
+  messageSeven: 'closing statement',
+  messageEight: 'thanks',
 }
 
 export default App;
